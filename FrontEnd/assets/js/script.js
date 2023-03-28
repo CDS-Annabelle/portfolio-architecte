ca948e6c47a29bbc790907a6fa738c2f7e323591
CDS <cdscontact@creation-de-site.eu>
2023-03-28 10:03:40 +0200

    update button

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -4,29 +4,27 @@ import {displayGalleryObjectsByCategoryId, addListenersToCategoryButtons, displa
 // Récupération des éléments du DOM - edit pour modifier & admin pour la topBar & token pour la connexion
 const edit = document.querySelector (".edit");
 const admin = document.querySelector (".admin");
+const log = document.getElementById("log");
+const categoryButtonsContainer = document.querySelector(".btns-category");
 const token = window.sessionStorage.getItem("token");
 
 // Affichage de la galerie et filtre par catégories sur les boutons
 const works = await getWorks();
-const uniqueWorks = [...new Set(works)];
 const categories = await getCategories();
 
-
 displayGalleryMenu(categories);
-//displayGalleryObjectsByCategoryId(works, "0");
-displayGalleryObjectsByCategoryId(uniqueWorks, "0");
+displayGalleryObjectsByCategoryId(works, "0");
 addListenersToCategoryButtons(works, categories);
-
 
 /* -- affichage du texte modifier lorsque l'utilisateur est connecté 
    -- déconnexion 
    -- affichage de la topBar admin
 */
-if (token != null)
-{
+if (token != null) {
     document.querySelector("body").style.paddingTop = "55px";
     edit.style.visibility = "visible";
     admin.style.visibility = "visible";
+    categoryButtonsContainer.style.display = "none";
     log.innerHTML = "Logout";
     log.addEventListener("click", () => {
         sessionStorage.removeItem("token");
@@ -35,7 +33,34 @@ if (token != null)
     });
 }
 
+// --- changement d'état des boutons de filtre ---
+const btns = document.querySelectorAll('.btn-name-category');
 
+btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // Récupère l'ID de la catégorie
+    const categoryId = btn.getAttribute('data-category-id');
+
+    // Réinitialise tous les boutons
+    btns.forEach(btn => {
+      btn.classList.remove('active');
+      btn.style.backgroundColor = '';
+      btn.style.color = '';
+    });
+
+    // Applique la classe 'active' au bouton actif
+    btn.classList.add('active');
+
+    // Modifie la couleur de fond et de texte du bouton actif
+    if (categoryId == 0) {
+      btn.style.backgroundColor = '#1D6154';
+      btn.style.color = 'white';
+    } else {
+      btn.style.backgroundColor = '#1D6154';
+      btn.style.color = '#fff';
+    }
+  });
+});
 
 
 
